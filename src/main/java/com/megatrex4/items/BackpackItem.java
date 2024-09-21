543d4d7bb59185d1451ad2368c0f4a75316219e7
Eugene <skyzgamer@outlook.com>
2024-09-21 19:23:37 +0300

    added ItemRegistry and BackpackItem class where we will define functions for backpacks added trinkets support 3.7.2+

---
+++ src/main/java/com/megatrex4/items/BackpackItem.java
@@ -0,0 +1,11 @@
+package com.megatrex4.items;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+
+public class BackpackItem extends Item {
+
+    public BackpackItem(Settings settings) {
+        super(settings);
+    }
+}
