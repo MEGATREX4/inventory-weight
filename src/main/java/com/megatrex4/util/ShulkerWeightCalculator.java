c3c12125fd01a2b26da135b72b0d4ced2c499026
Eugene <skyzgamer@outlook.com>
2024-09-25 17:40:23 +0300

    changed the dividing weight of the shaker from 0.85 to 0.95

--- src/main/java/com/megatrex4/util/ShulkerWeightCalculator.java
+++ src/main/java/com/megatrex4/util/ShulkerWeightCalculator.java
@@ -38,7 +38,7 @@ public class ShulkerWeightCalculator {
 
             float itemWeight = getItemWeight(itemStack) * itemStack.getCount();
 
-            totalWeight += (float) Math.max(InventoryWeightUtil.ITEMS, itemWeight * 0.85); // With modifier
+            totalWeight += (float) Math.max(InventoryWeightUtil.ITEMS, itemWeight * 0.95); // With modifier
             baseWeight += itemWeight; // Without modifier
         }
 
